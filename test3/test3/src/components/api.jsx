93840e4c98d62d77e8f77a8e3168fecdd9306ca6
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2025-03-19 19:37:16 +0530

    vs

--- test3/test3/src/components/api.jsx
+++ test3/test3/src/components/api.jsx
@@ -57,13 +57,14 @@
 
 
 
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import axios from 'axios';
 import { useEffect } from 'react';
 
 
 const api = () => {
     const [productData, setProductData] = useState([])
+    const [cartdata, setcartdata] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -77,12 +78,33 @@ const api = () => {
         }
         fetchData();
     }, []);
+
+    const addtocart = (item) => {
+        setcartdata([...cartdata, item])
+        console.log(cartdata)
+    }
+
+    const removeCart = (id) => {
+        const newItems = cartdata.filter((item) => item.id !== id)
+        setcartdata(newItems)
+    }
+
     return (
         <>
+            <h1>items of the cart : {cartdata.length}</h1>
+            {cartdata.map((item, index) => (
+                <div key={index}>
+                    <h5>{item.title}</h5>
+                    <p>{item.category}</p>
+                    <p>{item.price}</p>
+                    <button className='bg-red-500' onClick={() => removeCart(item.id)}>remove item</button>
+                </div>
+            ))}
             <div>
                 {productData.map((item, index) => (
                     <div key={index} className=''>
                         < img src={item.image} className='w-[100px]' alt={"product ${index}"} />
+                        <button className='bg-green-500 ' onClick={() => addtocart(item)}>Add to cart</button>
                     </div>
                 ))}
             </div>
